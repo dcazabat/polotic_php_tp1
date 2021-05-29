c1fdf8b1999c276359e023a02cc69baf6c40362d
unknown <dacazabat@gmail.com>
2021-05-29 20:35:35 -0300

    Cambio de Idea

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Cierra o Abre el SideBar
 $(document).ready(function () {
 
     $('#sidebarCollapse').on('click', function () {
@@ -5,3 +6,12 @@ $(document).ready(function () {
     });
 
 });
+
+//Redirige a las secciones con Scroll y los link tienen la class scrollLink
+/* $(document).ready(function () {
+    $("a.scrollLink").click(function (event) {
+        console.log("scroll")
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
+    });
+}); */
